bd8cb9eb6d46a49fdd83d2b1bf7d9d3dc39aae11
FernandoSigob <fernando.camarena@sigob.com.mx>
2025-07-14 23:35:22 -0600

    hotfix: Importación de crypto para railay

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+// PARA EVITAR PROBLEMAS DE RAILWAY
+if (!(global as any).crypto) {
+  (global as any).crypto = require('crypto');
+}
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
